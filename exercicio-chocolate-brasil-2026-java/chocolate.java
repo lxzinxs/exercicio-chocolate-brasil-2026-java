b1d38e0d728c07494762eead9445d435f047a7ce
Lucas Nery Miranda <lucasnerym@gmail.com>
2026-04-07 19:47:02 -0300

    Add files via upload

---
+++ exercicio-chocolate-brasil-2026-java/chocolate.java
@@ -0,0 +1,47 @@
+//scanner
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthDesktopIconUI;
+
+public class chocolate {
+    public static void main(String[] args) {
+        //teclado
+        Scanner teclado = new Scanner(System.in);
+
+        //entrada - variaveis
+        System.out.print("Escreva a porcentagem do teor do choclate: ");
+        double teor = teclado.nextDouble();
+
+        if(teor < 25){
+            System.out.println("ALERTA: Produto 'Fake'. Classificar como 'Sabor Chocolate");
+        } else if(teor > 25 && teor < 35){
+            System.out.println("Chocolate Comum (Padrão Nacional Atual)");
+        } else if(teor > 35){
+            System.out.println("Chocolate de Alta Qualidade (Padrão Internacional/UE)");
+        }
+        
+
+        System.out.println("O é chocolate ao_leite ou amargo? ");
+        String tipoChocolate = teclado.nextLine().toLowerCase();
+        System.out.println("Escreva o acucar do chocolate: ");
+        int acucar = teclado.nextInt();
+
+        if("ao_leite".equals(tipoChocolate) && acucar > 15){
+            System.out.println("Classificação: Doce de leite com cacau (Excesso de açúcar)");
+        } else {
+            System.out.println("Classificação: Chocolate balanceado");
+        }
+
+        System.out.println("Qual o preço do ovo de pascoa? ");
+        double valorOvoPascoa = teclado.nextDouble();
+
+        System.out.println("Qual a pureza do ovo? ");
+        int pureza = teclado.nextInt();
+
+        if(valorOvoPascoa > 100 || pureza < 5){
+            System.out.println("REJEITADO PARA EXPORTAÇÃO: Custo-benefício fora dos padrões");
+        } else {
+            System.out.println("APROVADO: Produto competitivo para o mercado externo");
+        }
+    }
+}
